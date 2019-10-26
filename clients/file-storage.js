23e017a513daaf6021d48e3edb2bc67ad151fb01
dimensi0n <erwan.roussel51@gmail.com>
2019-10-26 11:24:58 +0200

    Add file storage connection

--- clients/file-storage.js
+++ clients/file-storage.js
@@ -3,6 +3,9 @@ const axios = require('axios')
 class FileStorage {
     constructor (name, address, port) {
         this.name = name
+        this.address = address
+        this.port = port
+
         this.instance = axios.create({
             baseURL: `${address}${port ? ':'+port : ''}`,
             timeout: 1000,
@@ -13,6 +16,14 @@ class FileStorage {
     async Connect () {
         try {
             const signin = await this.instance.post('/signin', {username: this.name}, {})
+            this.instance = axios.create({
+                baseURL: `${this.address}${this.port ? ':'+this.port : ''}`,
+                timeout: 1000,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cookie': `token=${signin.data}`
+                }
+            })
         } catch (e) {
             console.error(e)
         }
